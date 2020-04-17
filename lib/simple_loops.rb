6895079ec3312ab1f4153a2e4d4eeb965278e848
Jordan Buysse <jdbuysse@gmail.com>
2020-04-17 00:56:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,31 @@ def loop_message_five_times(str)
     i -= 1
   end
 end
+
+def loop_message_n_times(str, n)
+  while n > 0 
+    puts str
+    n -= 1 
+  end
+end 
+
+def output_array(array)
+  i = 0
+  while i < array.length 
+    puts array[i]
+    i += 1
+  end
+end
+
+def return_string_array(array)
+  i = 0 
+  str = ''
+  while i < array.length
+    array[i].to_s
+    i += 1
+  end
+  array
+end
+  
+    
+
